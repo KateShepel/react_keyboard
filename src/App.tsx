449132d9aa286a133e6d765419163c37e5ee1976
Kate Shepel <MariyaKaterina@gmail.com>
2025-03-20 21:46:57 +0200

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 type State = {
   pressedKey: string;
-}
+};
 
 export class App extends React.Component<State> {
   state: State = {
@@ -16,27 +16,29 @@ export class App extends React.Component<State> {
       this.setState({
         ...this.state,
         pressedKey: event.key,
-      })
+      });
     }
-  }
+  };
 
   componentDidMount() {
     document.addEventListener('keyup', this.keyboardHendler);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', this.keyboardHendler)
+    document.removeEventListener('keyup', this.keyboardHendler);
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.pressedKey ?
-            <p className="App__message">The last pressed key is [{this.state.pressedKey}]</p> :
-            <p className='App__message'>Nothing was pressed yet</p>
-        }
+        {this.state.pressedKey ? (
+          <p className="App__message">
+            The last pressed key is [{this.state.pressedKey}]
+          </p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
       </div>
-    )
+    );
   }
-};
+}
